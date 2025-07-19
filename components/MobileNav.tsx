20f5cbcd5e16e3e3fb541dbcd9871871de305971
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2025-07-19 17:53:50 +0600

    navbar design

---
+++ components/MobileNav.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import Image from 'next/image'
+const MobileNav = () => {
+    return (
+        <section className='w-full max-w-[264px]'>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Image src="/icons/hamburger.svg" width={36} height={36} alt='hamburger icon' className='cursor-pointer sm:hidden' />
+                </SheetTrigger>
+                <SheetContent side="left" className='border-none bg-dark-1'>
+                    <SheetHeader>
+                        <SheetTitle>Are you absolutely sure?</SheetTitle>
+                        <SheetDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
+        </section>
+    )
+}
+
+export default MobileNav
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10 '>
+      <Link href="/" className='flex items-center gap-1'>
+      <Image src="/icons/logo.svg" width={32} height={32} alt='Video Calling logo' className='max-sm:size-10' />
+      <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Video Calling App</p>
+      </Link>
+      <div className='flex-between gap-5'>
+        {/* Clerk User mangement */}
+        <MobileNav />
+      </div>
+    </nav>
   )
 }
 
