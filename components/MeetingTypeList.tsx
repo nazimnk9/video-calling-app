b3caa455b2551e31e84698e4535230f94bfcdc2c
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2025-07-22 18:01:20 +0600

    meeting page design and add secrect key

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,8 +3,11 @@
 import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation'
+import MeetingModal from './MeetingModal'
 
-
+const createMeeting = ()=>{
+    
+}
 const MeetingTypeList = () => {
     const router = useRouter()
     const [meetingState, setMeetingState] = useState<"isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined>()
@@ -14,7 +17,7 @@ const MeetingTypeList = () => {
                 img="/icons/add-meeting.svg"
                 title="New Meeting"
                 description="Start an instant meeting"
-                handleClick={() => setMeetingState("isJoiningMeeting")}
+                handleClick={() => setMeetingState("isInstantMeeting")}
                 className="bg-orange-1"
             />
             <HomeCard
@@ -32,12 +35,20 @@ const MeetingTypeList = () => {
                 className="bg-purple-1"
             />
             <HomeCard
-                img="/icons/add-meeting.svg"
-                title="New Meeting"
-                description="Start an instant meeting"
+                img="/icons/join-meeting.svg"
+                title="Join Meeting"
+                description="Via invitation link"
                 handleClick={() => setMeetingState("isJoiningMeeting")}
                 className="bg-yellow-1"
             />
+            <MeetingModal
+            isOpen={meetingState === "isInstantMeeting"}
+            onClose={() => setMeetingState(undefined)}
+            title="Start an Instant Meeting"
+            className="text-center"
+            buttonText="Start Meeting"
+            handleClick={createMeeting}
+             />
         </section>
     )
 }
